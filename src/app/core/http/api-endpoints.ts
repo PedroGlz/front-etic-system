76405d71f0030cab221ebfef9767eb912c211c8e
Pedro Gonzalezm <gpedro.17.3f@gmail.com>
2026-06-26 22:53:54 -0600

    Configuración de ambientes fornt para consumir el back

--- src/app/core/http/api-endpoints.ts
+++ src/app/core/http/api-endpoints.ts
@@ -1,3 +1,5 @@
-export const API_BASE_URL = 'http://localhost:8080/api';
+import { environment } from '../../../environments/environment';
+
+export const API_BASE_URL = `${environment.apiBaseUrl}/api`;
 export const CATALOGS_API_URL = `${API_BASE_URL}/catalogos`;
 export const INSPECTIONS_API_URL = `${API_BASE_URL}/inspecciones`;
